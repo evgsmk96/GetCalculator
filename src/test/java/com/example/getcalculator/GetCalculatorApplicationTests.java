d14a19cff21269b07fc44da554bad41c2c731e89
evgsmk96 <evgsmk23@mail.ru>
2022-02-03 16:12:05 +0700

    Initial commit

---
+++ src/test/java/com/example/getcalculator/GetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
